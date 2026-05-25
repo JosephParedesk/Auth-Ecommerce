23317f59fa612b82769fca7e439b810eb9341ac3
Joseph Paredes <alejitoparedes2006@gmail.com>
2026-05-24 20:19:27 -0500

    update

--- src/main/java/com/ecommerce/auth/AuthApplication.java
+++ src/main/java/com/ecommerce/auth/AuthApplication.java
@@ -2,6 +2,10 @@ package com.ecommerce.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AuthApplication {
@@ -9,5 +13,17 @@ public class AuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
-
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
